63e3f88c0c27fa9946af5906cafe614764e7128e
Aron Elias HERRERA PONTE <aronherrera2009@gmail.com>
2014-05-11 18:21:21 +0200

    Add TemplateDB project

---
+++ TemplateDB/src/main/java/com/aehp/template/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.aehp.template.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.aehp.template")
+public class AppConfig {
+
+}
