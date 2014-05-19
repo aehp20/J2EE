b03a50fac891c14ba8a157b8806f4e036aba1642
Aron Herrera Ponte <ext-aherrera@parkeon.com>
2014-05-19 10:53:15 +0200

    Update AppInitializer

--- Template/src/main/java/com/aehp/template/initializer/AppInitializer.java
+++ Template/src/main/java/com/aehp/template/initializer/AppInitializer.java
@@ -5,36 +5,37 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import com.aehp.template.config.WebMvcConfig;
 
 public class AppInitializer implements WebApplicationInitializer {
 
 	private static final String CONFIG_LOCATION = "com.aehp.template.config";
-	private static final String MAPPING_URL = "/*";
+	private static final String MAPPING_URL = "/";
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-//		WebApplicationContext context = getContext();
-//		servletContext.addListener(new ContextLoaderListener(context));
+		WebApplicationContext context = getContext(servletContext);
+		servletContext.addListener(new ContextLoaderListener(context));
 //		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 //		dispatcher.setLoadOnStartup(1);
 //		dispatcher.addMapping(MAPPING_URL);
-		
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(WebMvcConfig.class);  
-        ctx.setServletContext(servletContext);    
-        
-        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        servlet.addMapping("/");  
-        servlet.setLoadOnStartup(1);  
+
+//		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(WebMvcConfig.class);
+//        ctx.setServletContext(servletContext);
+
+        Dynamic servlet = servletContext.addServlet("MyDispatcher", new DispatcherServlet(context));
+        servlet.addMapping(MAPPING_URL);
+        servlet.setLoadOnStartup(1);
 	}
 
-	private AnnotationConfigWebApplicationContext getContext() {
+	private AnnotationConfigWebApplicationContext getContext(ServletContext servletContext) {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
 		context.setConfigLocation(CONFIG_LOCATION);
+		context.setServletContext(servletContext);
 		return context;
 	}
 }
